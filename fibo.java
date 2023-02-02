e338e341a5ddc324582ba33274dc84da4ee53e12
Arjunvetal <arjunvetal.s.6065@gmail.com>
2023-02-02 17:09:21 +0530

    update file with prime no

--- fibo.java
+++ fibo.java
@@ -1,6 +1,6 @@
 class fibo{
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         int n1=0,n2=1,n3,i,count=20;
 
         System.out.println(n1+" "+n2);
@@ -14,6 +14,27 @@ class fibo{
          }  
          //upadate file 
 
-    
+    public static void PrimeNo()
+    {
+        int num=20, count;
+
+        for(int i=1; i<=num; i++)
+        {
+            count = 0;
+            for(int j=2; j<=i/2; j++)
+            {
+                if(i%j == 0)
+                {
+                    count++;
+                    break;
+                }
+            }
+
+            if(count == 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
     }
 }
